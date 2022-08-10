be50f8865d22b13066fa96526ac1557ab9a3af68
ShagMichail <shiga2001@mail.ru>
2022-08-10 20:51:33 +0300

    Initial Commit

---
+++ KHL/KHLApp.swift
@@ -0,0 +1,17 @@
+//
+//  KHLApp.swift
+//  KHL
+//
+//  Created by Михаил Шаговитов on 10.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct KHLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
